b64473b32f6718a3278c46c5fb849f9d69628595
Peter Zarnitz <peter.zarnitz@me.com>
2017-07-27 15:57:58 +0200

    Dienst Detail View working

--- bergwachttool/dienstplan/urls.py
+++ bergwachttool/dienstplan/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'dienstplan'
+
 urlpatterns = [
     # ex: /dienstplan/
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<dienstnummer>[0-9]+)/$', views.dienst_detail, name='dienst_detail'),
 ]
--- bergwachttool/dienstplan/views.py
+++ bergwachttool/dienstplan/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import Http404
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
 from .models import Dienst
@@ -14,3 +16,11 @@ def index(request):
         'dienst_list': dienst_list,
     }
     return HttpResponse(template.render(context, request))
+
+
+def dienst_detail(request, dienstnummer):
+    try:
+        dienst = Dienst.objects.get(dienstnummer=dienstnummer)
+    except Dienst.DoesNotExist:
+        raise Http404("Dienst existiert nicht")
+    return render(request, 'dienstplan/dienst_detail.html', {'dienst': dienst})
