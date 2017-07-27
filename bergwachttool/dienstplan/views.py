f775a0fcdcef5fef9f5c5493812c81becec9ee57
Peter Zarnitz <peter.zarnitz@me.com>
2017-07-27 15:42:47 +0200

    Add view for Dienstplan and CSS

--- bergwachttool/dienstplan/views.py
+++ bergwachttool/dienstplan/views.py
@@ -2,7 +2,15 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Dienst
 
 
 def index(request):
-    return HttpResponse("Dienstplan Index")
+    dienst_list = Dienst.objects.order_by('dienstbeginn')
+    template = loader.get_template('dienstplan/index.html')
+    context = {
+        'dienst_list': dienst_list,
+    }
+    return HttpResponse(template.render(context, request))
